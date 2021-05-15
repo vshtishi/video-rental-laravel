e251ac2783480a9548c49a822ad77d0f08f70a98
Sibora Sulaj <sibora.sulaj@fshnstudent.info>
2021-05-15 14:40:18 +0200

    Added administration design file

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/create', function () {
+    return view('create');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
